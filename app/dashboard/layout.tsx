cfead57277330055a379b8f634dbda553bdb53c8
Luca <mrluca.web@gmail.com>
2026-06-03 22:23:34 +0200

    fix mobile dashboard overflow

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
       <Navbar />
       <div className="flex">
         <DashboardSidebar />
-        <main className="flex-1 min-h-[calc(100vh-64px)]">
+        <main className="flex-1 min-h-[calc(100vh-64px)] min-w-0 overflow-x-hidden">
           {children}
         </main>
       </div>
